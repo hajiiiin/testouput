23707f9c198265add2808a7fb9f829cf84b8d31d
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2022-05-04 11:21:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setuptools.setup (
    description = 'oss- development class test output',
    author = 'hajiiiin',
    author_email = 'khajin@donga.ac.kr',
-   url = 'https://github.com/hajiiiin/testouput/',
-   download_url = 'https://github.com/sejin-chun/testouput/archive/refs/tags/v1.0.0.zip',
+   url = "https://github.com/hajiiiin/testouput/",
+   download_url = "https://github.com/hajiiiin/testouput/archive/refs/tags/v1.0.0.zip",
    install_requires = ['pytest'],
    long_description = 'oss development ...',
    long_description_content_type = 'text/markdown',
    classifiers = [                 
      "Programming Language :: Python :: 3",
-     "Operating System :: OS,
+     "Operating System :: OS Independent",
    ],
 )

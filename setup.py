8c8d96a7c8405064b58af89d008173ac278ce429
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2022-05-04 11:10:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup (
+   include_package_data = True ,
+   name = 'mycalc001',
+   version = '1.0.0',
+   description = 'oss- development class test output',
+   author = 'hajiiiin',
+   author_email = 'khajin@donga.ac.kr',
+   url = 'https://github.com/hajiiiin/testouput/',
+   download_url = 'https://github.com/sejin-chun/testouput/archive/refs/tags/v1.0.0.zip',
+   install_requires = ['pytest'],
+   long_description = 'oss development ...',
+   long_description_content_type = 'text/markdown',
+   classifiers = [                 
+     "Programming Language :: Python :: 3",
+     "Operating System :: OS,
+   ],
+)
